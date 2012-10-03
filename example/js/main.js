3105c8c80e469d7351973ada16b4ad514fd0c076
Barney Fox <barneyfoxuk@gmail.com>
2012-10-03 14:55:43 +0100

    added component file for Bower

---
+++ example/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    $('#sprite').animatedSprite({
+        fps: 6,
+        totalFrames: 6,
+        loop: 1,
+        autoStart: false,
+        resetAtEnd: true
+    });
+
+
+    //navigation
+    $(document).keydown(function(e){
+        //right
+        if (e.keyCode == 39) {
+            $('#sprite').animatedSprite("play");
+            var left = parseInt($('#sprite').css('left'));
+            left+=35;
+            console.log(left);
+            $('#sprite').animate({left: left}, 1000, "linear");
+        }
+    });
+
+});
+
