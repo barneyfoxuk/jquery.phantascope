fa2ca02c4652d675b12b7a91a3b8ace414591825
Barney Fox <barneyfoxuk@gmail.com>
2012-10-08 16:01:59 +0100

    added infinite loop and update method

--- example/js/main.js
+++ example/js/main.js
@@ -1,27 +1,25 @@
 $(document).ready(function() {
 
-    $('#sprite').animatedSprite({
-        fps: 6,
-        startPoint: [1,1],
-        endPoint: [6,1],
-        loop: 3,
-        autoStart: false,
-        resetAtEnd: true
+    var winWidth = $(window).width();
+
+    $.each($('.sprite'), function(i,n) {
+        $(n).animatedSprite({
+            fps: 30,
+            startPoint: [1,1],
+            endPoint: [8,4],
+            layout: [8, 8, 8, 8],
+            loop: "*",
+            autoStart: true,
+            resetAtEnd: true
+        })
     });
 
-    $('#sprite').animatedSprite("play", {startPoint: [1,2], endPoint: [6,2]});
+    //$('#sprite').animatedSprite("play", {startPoint: [1,2], endPoint: [6,2]});
 
-    //navigation
-    $(document).keydown(function(e){
-        //right
-        if (e.keyCode == 39) {
-
-
-            var left = parseInt($('#sprite').css('left'));
-            left+=35;
-            console.log(left);
-            $('#sprite').animate({left: left}, 1000, "linear");
-        }
+    $(document).mousemove(function(e) {
+        var xPercenage = e.pageX/winWidth;
+        var fps = Math.floor((xPercenage*200)+30);
+        $('.sprite').animatedSprite("update", {fps: fps});
     });
 
 });
--- jquery.animatedSprite.js
+++ jquery.animatedSprite.js
@@ -5,7 +5,7 @@
      *  Private Methods
      */
 
-     var nextFrame2 = function($this) {
+     var onNextFrame = function($this) {
 
         var data = $this.data('animatedSprite'),
             currentColumn = data.currentPoint[0],
@@ -14,12 +14,11 @@
             endPoint = data.endPoint;
 
 
-        console.log("data.currentPoint", data.currentPoint, "data.endPoint", data.endPoint);
+        //console.log("data.currentPoint", data.currentPoint, "data.endPoint", data.endPoint);
 
 
         //end of animation
         if(currentColumn == data.endPoint[0] && currentRow == data.endPoint[1]) {
-            console.log("end of animation");
 
             $this.removeClass("animating");
 
@@ -29,8 +28,7 @@
             }
 
             //check if we need to loop
-            if(data.currentLoopIndex < data.loop) {
-                console.log("we need to loop");
+            if(data.loop === "*" || data.currentLoopIndex < data.loop) {
                 data.currentLoopIndex++;
                 $this.animatedSprite("play");
             }
@@ -47,77 +45,42 @@
         }
         //is end of row
         else if(currentColumn == data.layout[currentRow-1]) {
-            console.log(data.layout[currentRow]);
-            console.log("is end of row");
             //is last row
             if(currentRow == data.layout.length) {
-                console.log("is last row");
             }
             //is not last row
             else {
-                console.log("is not lst row");
                 //move to next row
                 data.currentPoint = [1, currentRow+1];
                 renderCurrentFrame($this);
-                bufferNextFrame($this);
+                nextFrame($this);
             }
         }
         //is not end of row
         else {
-            console.log("currentRow", currentRow);
-            console.log("currentColumn", currentColumn);
             data.currentPoint = [currentColumn+1, currentRow];
             renderCurrentFrame($this);
-            bufferNextFrame($this);
+            nextFrame($this);
         }
 
         $this.data('animatedSprite', data);
     };
 
-    var bufferNextFrame = function($this) {
-        var data = $this.data('animatedSprite');
 
-        data.interval = setTimeout(function() {
-            nextFrame2($this);
-        }, data.tickTime);
-    }
-
-    var nextFrame = function($this) {
-
-        var data = $this.data('animatedSprite');
-
-        if(data.currentFrame < data.totalFrames) {
-            data.currentFrame++;
-            renderCurrentFrame($this);
-            data.interval = setTimeout(function() {
-                nextFrame($this);
-            }, data.tickTime);
-        } else {
-            if(data.repeat == data.currentLoop) {
-                $this.removeClass("animating");
-
-                if(data.resetAtEnd === true) {
-                    data.currentFrame = 1;
-                    renderCurrentFrame($this);
-                }
-            } else {
-                data.currentLoop++;
-                data.currentFrame = 1;
-                renderCurrentFrame($this);
-                data.interval = setTimeout(function() {
-                    nextFrame($this);
-                }, data.tickTime);
-            }
-        }
-
-        $this.data('animatedSprite', data);
-    };
 
     var renderCurrentFrame = function($this) {
         var data = $this.data('animatedSprite');
         $this.css("backgroundPosition", "-"+((data.currentPoint[0]-1)*$this.outerWidth())+"px "+((data.currentPoint[1]-1)*-$this.outerHeight())+"px");
     };
 
+
+    var nextFrame = function($this) {
+        var data = $this.data('animatedSprite');
+
+        data.interval = setTimeout(function() {
+            onNextFrame($this);
+        }, Math.floor(1000/data.fps));
+    };
 
 
     /*
@@ -145,7 +108,7 @@
                         autoStart: true,
                         layout: [6,6,2],
                         startPoint: [1,1],
-                        endPoint: [6,2],
+                        endPoint: [10,6],
                         repeat: 3,
                         reverseAtEnd: false,
                         resetAtEnd: true,
@@ -153,7 +116,7 @@
 
                     }, options);
 
-                    data.tickTime = Math.floor(1000/data.fps);
+                    //data.tickTime = Math.floor(1000/data.fps);
                     data.currentLoopIndex = 1;
 
                     $(this).data('animatedSprite', data);
@@ -167,7 +130,6 @@
         },
 
         play : function(options) {
-            console.log("play", options);
 
             var $this = $(this);
             var data = $this.data('animatedSprite');
@@ -176,24 +138,31 @@
                 data = $.extend(data, options);
 
             data.currentPoint = data.startPoint;
-            console.log(data.currentPoint);
 
             $(this).data('animatedSprite', data);
 
             if($this.hasClass("animating") === false) {
 
-
                 $this.addClass("animating");
 
                 //var tickTime = Math.floor(1000/$(this).data('animatedSprite').fps);
 
-                data.interval = setTimeout(function() {
-                    //$this.animatedSprite("nextFrame");
-                    nextFrame2($this);
-                }, data.tickTime);
+                nextFrame($this);
 
                 $(this).data('animatedSprite', data);
             }
+        },
+
+        update : function(options) {
+
+            var $this = $(this);
+            var data = $this.data('animatedSprite');
+
+            if(options)
+                data = $.extend(data, options);
+
+             $(this).data('animatedSprite', data);
+
         },
 
         destroy : function( ) {
